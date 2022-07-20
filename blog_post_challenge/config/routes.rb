7e8544c035fece7a4e334370bb9a4934a0ac2733
alex <sun@suns-MacBook-Air.local>
2022-07-20 19:06:51 -0400

    upload

--- blog_post_challenge/config/routes.rb
+++ blog_post_challenge/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get 'blogs' => 'blog#index'
+  get 'blogs' => 'blog#index', as: 'blogs'
+  get 'blogs/:id' => 'blog#show', as: 'blog'
+  root 'blog#index'
 
 
   # Defines the root path route ("/")
